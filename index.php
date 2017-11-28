eae95687b8eec55a562a956463d6a2c0ace425f5
Argonder <jean-romain59@hotmail.fr>
2017-11-28 10:28:06 +0100

    Depot Initial de mon Theme

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+	Ici , je suis sur le fichier par defaut pour tout theme WP
+	il est l'un des deux obligatoire ( l'autre etant le style.css)
+	il est utilisé pour afficher une page si aucun autre fichier n'est disponible
+	*/
+	get_header();
+	get_footer();
+
+
+
+?>
+
+
+        
+       
